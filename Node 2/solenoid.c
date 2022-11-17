a233dda126ca014899db5b8ecb26dc8ad67a30c0
Mmmm <72460751+emmall99@users.noreply.github.com>
2022-11-17 22:27:59 +0100

    Add files via upload

---
+++ Node 2/solenoid.c
@@ -0,0 +1,23 @@
+/*
+ * solenoid.c
+ *
+ * Created: 17.11.2022 21:59:11
+ *  Author: emmalle
+ */ 
+
+#include "solenoid.h"
+#include "sam.h"
+
+void solenoid_init(){
+	REG_PIOC_PER |= PIO_PC9;
+	REG_PIOC_OER |= PIO_PC9;
+	REG_PIOC_PUDR |= PIO_PC9;
+	
+}
+
+/*void solenoid_punch(){
+	if (p13 high)
+	{
+		
+	}
+}*/
---
+++ Node 2/solenoid.h
@@ -0,0 +1,16 @@
+/*
+ * solenoid.h
+ *
+ * Created: 17.11.2022 21:59:23
+ *  Author: emmalle
+ */ 
+
+
+#ifndef SOLENOID_H_
+#define SOLENOID_H_
+
+void solenoid_init();
+
+
+
+#endif /* SOLENOID_H_ */
