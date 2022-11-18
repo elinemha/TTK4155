a99aa9fe51daca98e2564a0e997b3923e4dec648
Mmmm <72460751+emmall99@users.noreply.github.com>
2022-11-18 15:18:28 +0100

    Update solenoid.c

--- Node 2/solenoid.c
+++ Node 2/solenoid.c
@@ -12,12 +12,17 @@ void solenoid_init(){
 	REG_PIOC_PER |= PIO_PC9;
 	REG_PIOC_OER |= PIO_PC9;
 	REG_PIOC_PUDR |= PIO_PC9;
-	
 }
 
-/*void solenoid_punch(){
-	if (p13 high)
+void solenoid_punch(){
+	if (punch == 1)
 	{
-		
+		REG_PIOC_CODR |= PIO_PC9;
 	}
-}*/
+	else if (punch == 0){
+		REG_PIOC_SODR |= PIO_PC9;
+	}
+}
+//Define punch = joy_pos.data[4] in main
+//Call function
+//Get the button to work. It is still 0 all the time.  from NOde 1. Check with oscilloscope?
