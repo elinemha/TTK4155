9464538226096ab54d12bffe975854f34d7964cc
Rohan Khan <56409875+rohankhan97@users.noreply.github.com>
2022-11-19 11:41:29 +0100

    Final main

--- Node 2/main.c
+++ Node 2/main.c
@@ -19,6 +19,8 @@
 #include "motor.h"
 #include "delay_due.h"
 
+#define SYS_FREQ 84000000
+
 #define CAN_BR_1 0x00290165
 
 #define NOT_RST PIO_PD1
@@ -29,46 +31,44 @@ int map(int x, int in_min, int in_max, int out_min, int out_max) {
 
 int main(void)
 {
-    /* Initialize the SAM system */
-	systick_setup(84000000);
+    /* Initializations */
+	systick_setup(SYS_FREQ);
     SystemInit();
-	WDT->WDT_MR |= WDT_MR_WDDIS;
+	WDT->WDT_MR |= WDT_MR_WDDIS; // Disabling watchdog timer on ATSAM
 	configure_uart();
 	can_init_def_tx_rx_mb(CAN_BR_1);
 	PWM_init();
-	adc_init();
-	
+	adc_init();	
 	dac_init();
 	motor_init();
 	controller_init();
 	solenoid_init();
 	
 		
-	int16_t enc_pos;
-	int16_t motor_pos;
+	int16_t enc_pos; // variable for reading encoder position
+	int16_t motor_pos; // variable for setting motor speed, which is determined by using PID Controller
 	
-	MOTOR_DIRECTION md = LEFT;
-	
-	int score = 0;
-	uint8_t left_slider_pos, left_slider_pos_previous;
+	int score = 0; // variable for scoring
+	uint8_t left_slider_pos, left_slider_pos_previous; // left slider positions for controlling the motor position
 	
 	printf("STARTING... ");
 	
-	set_direction(LEFT);
+	MOTOR_DIRECTION md = LEFT;
+	
+	/* Initialization routine to bring motor to the left as starting position */
+	set_direction(LEFT); // Initialize motor direction as left
 	set_speed(0x0F00);
 	us_delay(2000000);
 	
     while (1) 
     {
-		CAN0_Handler();
+		CAN0_Handler(); // CAN Handler for reading CAN data
 		
 		int punch = message.data[4];
-		solenoid_punch(punch);
+		solenoid_punch(punch); // Push Solenoid to hit the ball
 		
 		left_slider_pos = message.data[2];
-		int joy_value_x = map(message.data[0]-5, 0, 255, 2080, 920);
-		// int16_t joy_speed = map(message.data[0]-50, 0, 255, 0x0100, 0xFFF);
-		
+		int joy_value_x = map(message.data[0]-5, 0, 255, 2080, 920); // Read Joystick X position and map it to PWM
 		PWM_setDC(joy_value_x);
 		
 		if (left_slider_pos - abs(enc_pos) < 0)
@@ -104,14 +104,14 @@ int main(void)
 		
 		us_delay(100000);	
 				
-		uint16_t IR_value = read_IR();
+		uint16_t IR_value = read_IR(); // Read the IR value
 		int goal = goal_scored(IR_value);
-		/*
+		
 		if(goal==1){
 			printf("         GOALLL!!!     ");
 			score += 1;
 		}
 		printf("Score: %d \n", score);
-		*/	
+		
    }	
 }
