1e0eae5a58bb11b0b5ae9d2a15e61303740d3a88
elinemha <91148359+elinemha@users.noreply.github.com>
2022-11-19 10:31:34 +0100

    cleanup/ added comments to controller code

--- Node 2/controller.c
+++ Node 2/controller.c
@@ -17,6 +17,7 @@
 // D0-D7 ARE PC1-8
 #define MJ2_PORT 0x1FE
 
+//scaled max speed
 uint8_t maxSpeed = 255;
 
 //PID parameters
@@ -31,32 +32,33 @@ float prev_error;
 float derivative;
 float sum_error;
 
-int16_t maxOutput = 15000; //fact check
+int16_t maxOutput = 15000; 
 
 volatile uint32_t ms_ticks = 0;
 
 void systick_setup(uint32_t sys_freq) {
-	SysTick->LOAD = (sys_freq / 1000000U) - 1U; //1ms tick
-	SysTick->VAL = 0x00; //explicitly set start value (undefined on reset)
+	SysTick->LOAD = (sys_freq / 1000000U) - 1U; 											//1ms tick
+	SysTick->VAL = 0x00; 																	//explicitly set start value (undefined on reset)
 	SysTick->CTRL |= (1U << SysTick_CTRL_TICKINT_Pos) | (1U << SysTick_CTRL_CLKSOURCE_Pos); //enable systick interrupt, source processor clock
-	SCB->SHP[8] = 0U; //set SysTick interrupt priority (default: 0, the highest)
-	SysTick->CTRL |= (1U << SysTick_CTRL_ENABLE_Pos); //enable SysTick
+	SCB->SHP[8] = 0U;																		//set SysTick interrupt priority (default: 0, the highest)
+	SysTick->CTRL |= (1U << SysTick_CTRL_ENABLE_Pos); 										//enable SysTick
 }
 
 void SysTick_Handler(void)
 {
 	ms_ticks++;
 }
+
 uint32_t getMillis(void)
 {
 	return ms_ticks;
 }
+
 void us_delay(uint32_t delay)
 {
 	uint32_t prevMillis = getMillis() + delay;
 	while (getMillis() != prevMillis);
 }
-
 
 void dac_init(){
 	REG_PMC_PCER1 |= PMC_PCER1_PID38;
@@ -97,18 +99,17 @@ void set_direction(MOTOR_DIRECTION direction)
 int16_t read_encoder()
 {
 	int16_t enc_data;
-	REG_PIOD_CODR = NOT_OE;
-	REG_PIOD_CODR = SEL;
-	us_delay(25);
-	uint8_t high_byte = (uint8_t) ((REG_PIOC_PDSR & MJ2_PORT) >> 1);
-	
-	REG_PIOD_SODR = SEL;	
-	us_delay(25);
-	uint8_t low_byte = (uint8_t) ((REG_PIOC_PDSR & MJ2_PORT) >> 1);
+	REG_PIOD_CODR = NOT_OE;												//Set !OE low, to sample and hold the encoder value
+	REG_PIOD_CODR = SEL;												//Set SEL low to output high byte
+	us_delay(25);														//Wait approx. 20 microseconds for output to settle
+	uint8_t high_byte = (uint8_t) ((REG_PIOC_PDSR & MJ2_PORT) >> 1);	//Read MJ2 to get high byte
+	REG_PIOD_SODR = SEL;												//Set SEL high to output low byte
+	us_delay(25);														//Wait approx. 20 microseconds
+	uint8_t low_byte = (uint8_t) ((REG_PIOC_PDSR & MJ2_PORT) >> 1);		//Read MJ2 to get low byte
 	
 	REG_PIOD_CODR |= PIO_CODR_P1;
 	REG_PIOD_SODR |= PIO_SODR_P1;
-	PIOD->PIO_SODR = NOT_OE;
+	PIOD->PIO_SODR = NOT_OE;											//Set !OE to high
 	
 	enc_data = ((high_byte<<8) | low_byte);
 	
@@ -116,6 +117,7 @@ int16_t read_encoder()
 		enc_data = -((uint16_t)(~enc_data)+1);
 	}
 	
+	//limit encoder to go outside of range 0-255
 	enc_data = (int16_t) (((enc_data*255)/9000));
 	if (enc_data>0)
 	{
@@ -129,6 +131,7 @@ int16_t read_encoder()
 	return enc_data;
 }
 
+
 void set_speed(int16_t speed_joystick)
 {
 	REG_DACC_CDR = speed_joystick;
@@ -140,7 +143,7 @@ void reset_encoder(){
 	PIOD->PIO_SODR |= NOT_RST;
 }
 
-
+//storing initial values for PID controller
 void controller_init()
 {
 	kp = 6;
@@ -150,21 +153,19 @@ void controller_init()
 	reference   = 0;
 	integral    = 0;
 	prev_error  = 0;
-
+	//sum_error = 0;
 }
 
 // PID controller for motor position
 int16_t position_controller(int16_t enc_pos, uint8_t slider_pos)
 {
 	reference = slider_pos;
-	int16_t error = reference - abs(enc_pos);				// position = y
+	int16_t error = reference - abs(enc_pos);		// error = (slider position) - (encoder position), e = r - y
 	sum_error += error;
 	integral = sum_error * Time_sample;
 	derivative = (error-prev_error)/Time_sample;
 	
 	printf("Error: %d \n", error);
-	//int16_t sum_error = 0;
-	//sum_error += error;
 	
 	if (error<1)
 	{
@@ -174,8 +175,8 @@ int16_t position_controller(int16_t enc_pos, uint8_t slider_pos)
 	int32_t u_p = kp*error;
 	int32_t u_i = ki*integral;
 	int32_t u_d = kd*derivative;
-	
-	int16_t u_t = u_p + u_i + u_d;				//u_t output from PI sum operation, u[n] = Kp*e[n]+T*Ki*sum_error+Kd*(error-error_prev)/T
+	//u_t output from PI sum operation
+	int16_t u_t = u_p + u_i + u_d;					// u[n] = Kp*e[n]+T*Ki*sum_error+Kd*(error-error_prev)/T
 	
 	// avoiding integral windup
 	if((abs(sum_error) > maxOutput) && sum_error >= 0){
@@ -192,8 +193,8 @@ int16_t position_controller(int16_t enc_pos, uint8_t slider_pos)
 	else if ((abs(u_t) > maxOutput) && u_t <= 0){
 		u_t = -maxOutput;
 	}
-	
-	prev_error = error;         //stores error for next run
+	//stores error for next run
+	prev_error = error;         
 		
 	return abs(u_t);
 }
